421041faeab5537865c06383854fbbc9854e11f9
Abhinandan Mittal <abhmitta@amazon.com>
2025-10-15 20:29:34 +0530

    Adding headers in requests so it seems more legit

--- dnb-exam-scraper/app.py
+++ dnb-exam-scraper/app.py
@@ -50,7 +50,13 @@ def send_message(message, debug):
 
 def scrap_website():
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Referer': 'https://natboard.edu.in/'
+        }
+
+        response = requests.get(url, params=headers)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
